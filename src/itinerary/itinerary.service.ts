4b2401c7d00a37fa6d2b025fed3b83aae432e3ac
angkoonhwee <akoonhwee@gmail.com>
2022-03-13 02:38:38 +0800

    asdfasdf

--- src/itinerary/itinerary.service.ts
+++ src/itinerary/itinerary.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { itineraryDTO } from './model/itinerary.dto';
 
 @Injectable()
 export class ItineraryService {
-    constructor() {}
+    public async getItineraryById(id: string) {
+        try {
+            
+        } catch (err) {
+            
+        }
+    }
 }
